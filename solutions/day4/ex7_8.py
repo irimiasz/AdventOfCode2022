b58207f7f50db8cb19ff1faadc1cb4bcada8125b
Radoslaw Kuzicki <rkuzicki@gmail.com>
2022-12-04 16:02:16 +0100

    Day4

---
+++ solutions/day4/ex7_8.py
@@ -0,0 +1,20 @@
+def parse_input(line: str) -> dict[int, int]:
+    data = list(map(int, line.replace("\n", "").replace(",", " ").replace("-", " ").split(" ")))
+    return {data[0]: data[1], data[2]: data[3]}
+
+
+def solution() -> tuple[int, int]:
+    fully_covered = 0
+    partially_covered = 0
+    with open("input.txt", "r") as input_file:
+        for line in input_file.readlines():
+            coverage = parse_input(line)
+            if min(coverage.values()) >= max(coverage.keys()):
+                partially_covered += 1
+            if coverage[max(coverage.keys())] <= coverage[min(coverage.keys())]:
+                fully_covered += 1
+    return fully_covered, partially_covered
+
+
+if __name__ == "__main__":
+    print(solution())
